1f72f2c4855a88180b4912194cf9e98a9114f6ed
Mit Kotak <mitkotak0305@gmail.com>
2025-11-26 00:36:26 +0000

    added colab numbers

--- scripts/benchmark_ase_mace.py
+++ scripts/benchmark_ase_mace.py
@@ -18,14 +18,14 @@ from matbench_speed.benchmark import benchmark
 from mace.calculators.foundations_models import mace_mp
 
 calculators = {
-    "MACE-MP-0-no-kernels": mace_mp(
-        model="medium-0b3",
-        default_dtype="float32",
-        device="cuda",
-        compile_mode="default",
-        fullgraph=False,
-        enable_cueq=False,
-    ),
+    #"MACE-MP-0-no-kernels": mace_mp(
+    #    model="medium-0b3",
+    #    default_dtype="float32",
+    #    device="cuda",
+    #    compile_mode="default",
+    #    fullgraph=False,
+    #    enable_cueq=False,
+    #),
     "MACE-MP-0": mace_mp(
         mode="medium-0b3",
         default_dtype="float32",
